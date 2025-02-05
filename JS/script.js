cbef19b363621eefdaf2d9063555ec08e48c9e33
Atul Gunjal <gunjalatul2015@gmail.com>
2025-02-05 11:01:00 +0530

    emrge

--- JS/script.js
+++ JS/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
        // This comment is just for checking that how to resolve git merge conflicts.
   // As I have a 'feature' branch and in this branch this comment is not available.
   // Now we have created and commited this comment in bugfix branch.
-      return color;
+      // return color;
     }
   });
  
