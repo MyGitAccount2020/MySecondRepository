b271f527e533e9f6db65842b54c68910abeb23d5
Atul Gunjal <gunjalatul2015@gmail.com>
2025-02-05 11:02:01 +0530

    removed lines

--- JS/script.js
+++ JS/script.js
@@ -9,10 +9,6 @@ $(document).ready(function() {
       for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
       }
-       // This comment is just for checking that how to resolve git merge conflicts.
-  // As I have a 'feature' branch and in this branch this comment is not available.
-  // Now we have created and commited this comment in bugfix branch.
-      // return color;
     }
   });
  
