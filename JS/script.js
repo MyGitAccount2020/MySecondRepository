678c2d258dfba05d09b86c9153abe38375a13bc0
Atul Gunjal <gunjalatul2015@gmail.com>
2025-02-05 11:10:45 +0530

    modiefied

--- JS/script.js
+++ JS/script.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
       $("#box").css("background-color", getRandomColor());
     });
   
-   
+    function getRandomColor() {
+      let letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color; //this is comment.
+    }
+  });
+  
