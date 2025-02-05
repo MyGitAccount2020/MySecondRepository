2781d5689ff1cd99131a5a5b65eebe1ec5e2ad16
Atul Gunjal <gunjalatul2015@gmail.com>
2025-02-05 10:57:48 +0530

    changes some lines to know merge conflicts concept.

--- JS/script.js
+++ JS/script.js
@@ -9,9 +9,10 @@ $(document).ready(function() {
       for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
       }
+       // This comment is just for checking that how to resolve git merge conflicts.
+  // As I have a 'feature' branch and in this branch this comment is not available.
+  // Now we have created and commited this comment in bugfix branch.
       return color;
     }
   });
-  // This comment is just for checking that how to resolve git merge conflicts.
-  // As I have a 'feature' branch and in this branch this comment is not available.
-  // Now we have created and commited this comment in bugfix branch.
+ 
